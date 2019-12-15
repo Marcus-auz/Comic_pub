d8a11fb7e464b6018b2be1636a8f218d6ef618f9
Auz <oo7marcusaurelius@gmail.com>
2019-12-15 16:26:45 +0530

    series routes added

--- api/api.js
+++ api/api.js
@@ -1,11 +1,15 @@
 const express =require('express');
 //imported the artist router
 const artistsrouter=require('./artists')
+//imported series router
+const seriesrouter=require('./series');
 //created instance of express router
 const apirouter=express.Router();
 
 //mounted the artistrouter to all /artist on apirouter
 apirouter.use('/artists',artistsrouter);
+//mounting the series router on all /series router
+seriesrouter.use('/series',seriesrouter);
 
 //exported the api router
 module.exports=apirouter;
---
+++ api/series.js
@@ -0,0 +1,85 @@
+const express=require('express');
+const seriesrouter=express.Router();
+const sqlite3=require('sqlite3');
+
+const db=new sqlite3.Database(process.env.TEST_DATABASE ||'./database.sqlite');
+
+//retrieve specefied series from database and attach it to request object
+seriesrouter.param('seriesId',(req,res,next,seriesId)=>{
+    const sql='SELECT * FROM Series WHERE Series.id=$seriesId';
+    const value={$seriesId:seriesId};
+    db.get(sql,value,(err,series)=>{
+        if(error){
+            next(error);
+        }else if(series){
+            req.series=series;
+            next();
+        }else{
+            res.sendStatus(404)
+        }
+    });
+});
+
+//retrieve all series (return error if not such table)
+seriesrouter.get('/',(req,res,next)=>{
+    db.all('SELECT * FROM Series',(err,series)=>{
+        if(err){
+            next(err);
+        }else{
+            res.status(200).json({series:series});
+        }
+    });
+});
+//retrieve the series on request object
+seriesrouter.get('/:seriesId',(req,res,next)=>{
+    res.status(200).json({series:req.series});
+});
+
+//if name and desc field are missing then send error else add them in the database nad them retrive them too
+seriesrouter.post('/',(req,res,next)=>{
+    const name=req.body.series.name;
+    const description=req.body.series.description;
+    if(!name || !description){
+        return res.statusCode(400);
+    }
+    const sql=('INSERT INTO Series(name,description)VALUES($name,$description');
+    const values={
+        $name:name,
+        $description:description
+    };
+    db.run(sql,values,function(error){
+        if(error){
+            next(error);
+        }else{
+            db.get(`SELECT * FROM Series Where Series.id=${this.lastID}`,(err,series)=>{
+                res.status(201).json({series:series});
+            });
+        }
+    });
+
+});
+
+seriesrouter.put('/:seriesId',(req,res,next)=>{
+    const name=req.body.series.name;
+    const description=req.body.series.description;
+    if(!name || !description){
+        return res.sendStatus(400);
+    }
+    const sql='UPDATE Series SET name=$name,description=$description'+'WHERE Series.id=$series.Id';
+    const values={
+        $name:name,
+        $description:description,
+        $seriesId:req.param.seriesId}
+    db.run(sql,values,(error)=>{
+        if(error){
+            next(error);
+        }else{
+            db.get(`SELECT * FROM Series WHERE Series.id=${req.param.seriesId}`,(error,series)=>{
+                res.status(200).json({series:series});
+            });
+        }
+    });
+
+});
+
+module.exports=seriesrouter;
