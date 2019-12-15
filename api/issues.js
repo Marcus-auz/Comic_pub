17964448c8c6ab692956d081ed8330c07b88f5e0
Auz <oo7marcusaurelius@gmail.com>
2019-12-15 23:57:11 +0530

    all test running,bug fixed

--- api/issues.js
+++ api/issues.js
@@ -1,126 +1,129 @@
-const express=require('express');
-const issuerouter=express.Router({mergeParams:true});
-const sqlite3=require('sqlite3');
-const db=new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+const express = require('express');
+const issuerouter = express.Router({mergeParams: true});
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 //if issue with the supplied issueId exist
-issuerouter.param('issueId',(req,res,next,issueId)=>{
-    const sql='SELECT * FROM Issue WHERE Issue.id=$issueId ';
-    const values={
-        $issueId:issueId
-    };
-    db.get(sql,values,(err,issue)=>{
-        if(err){
-            next(err);
-        }else if(issue){
-            next();
-        }else{
-            res.sendStatus(404);
-        }
-    });
+issuerouter.param('issueId', (req, res, next, issueId) => {
+  const sql = 'SELECT * FROM Issue WHERE Issue.id = $issueId';
+  const values = {$issueId: issueId};
+  db.get(sql, values, (error, issue) => {
+    if (error) {
+      next(error);
+    } else if (issue) {
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
 });
+
 //get all issues of a specified series
 issuerouter.get('/', (req, res, next) => {
-    const sql = 'SELECT * FROM Issue WHERE Issue.series_id = $seriesId';
-    const values = { $seriesId: req.params.seriesId};
-    db.all(sql, values, (error, issues) => {
-      if (error) {
-        next(error);
-      } else {
-        res.status(200).json({issues: issues});
-      }
-    });
+  const sql = 'SELECT * FROM Issue WHERE Issue.series_id = $seriesId';
+  const values = { $seriesId: req.params.seriesId};
+  db.all(sql, values, (error, issues) => {
+    if (error) {
+      next(error);
+    } else {
+      res.status(200).json({issues: issues});
+    }
   });
+});
 // add the new issue in the database and should return error if not enough data or if the artist is missing
-issuerouter.post('/',(req,res,next)=>{
-    const name=req.body.issue.name;
-    const issueNumber=req.body.issue.issueNumber;
-    const publicationDate=req.body.issue.publicationDate;
-    const artistId=req.body.issue.artistId;
-    const artistSql='SELECT * FROM Artist WHERE Artist.id=$artistid';
-    const artistValues={$artistId:artistId};
-    db.get(artistSql,artistValues,(error,artist)=>{
-        if(error){
-            next(error);
-        }else{
-            //error if not enough data
-            if(!name || !issueNumber || !publicationDate || !artist){
-                return res.sendStatus(400);
-            }
-            //query to update the database
-            const sql='INSERT INTO Issue(name,issue_Number,publication_date,artist_Id,series_Id) VALUES($name,$issueNumber,$publicationDate,$artistId,$seriesId)';
-            const values={
-                $name:name,
-                $issueNumber:issueNumber,
-                $publicationDate:publicationDate,
-                $artistId:artistId,
-                $seriesId:req.param.seriesId
-            };
-            db.run(sql,values,function(err){
-                if(err){
-                    next(err);
-                }else{
-                    db.get(`SELECT * FROM Issue WHERE Issue.id=${this.lastID} `,(error,issue)=>{
-                        res.status(201).json({issue:issue});
-                    });
-                }
-        
+issuerouter.post('/', (req, res, next) => {
+  const name = req.body.issue.name,
+        issueNumber = req.body.issue.issueNumber,
+        publicationDate = req.body.issue.publicationDate,
+        artistId = req.body.issue.artistId;
+  const artistSql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
+  const artistValues = {$artistId: artistId};
+  db.get(artistSql, artistValues, (error, artist) => {
+    if (error) {
+      next(error);
+    } else {
+        //error if not enough data
+      if (!name || !issueNumber || !publicationDate || !artist) {
+        return res.sendStatus(400);
+      }
+      //query to update the database
+      const sql = 'INSERT INTO Issue (name, issue_number, publication_date, artist_id, series_id)' +
+          'VALUES ($name, $issueNumber, $publicationDate, $artistId, $seriesId)';
+      const values = {
+        $name: name,
+        $issueNumber: issueNumber,
+        $publicationDate: publicationDate,
+        $artistId: artistId,
+        $seriesId: req.params.seriesId
+      };
+
+      db.run(sql, values, function(error) {
+        if (error) {
+          next(error);
+        } else {
+          db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`,
+            (error, issue) => {
+              res.status(201).json({issue: issue});
             });
         }
-    }); 
+      });
+    }
+  });
 });
 
 //update the issue in db and return the newly updated series
-issuerouter.put('/:issueId',(req,res,next)=>{
-    const name=req.body.issue.name;
-    const issueNumber=req.body.issue.issueNumber;
-    const publicationDate=req.body.issue.publicationDate;
-    const artistId=req.body.issue.artistId;
-    const artistSql='SELECT * FROM Artist WHERE Artist.id=$artistid';
-    const artistValues={$artistId:artistId};
-    db.get(artistSql,artistValues,(err,artist)=>{
-        if(err){
-            next(err);
-        }else{
-            if(!name || !issueNumber || !publicationDate || !artist){
-                return res.statusCode(400);
-            }
-            const sql='UPDATE Issue SET name=$name,issue_number=$issueNumber'+
-                        'publication_date=$publicationDate,artistId=$artist_Id'+
-                         'WHERE Issue.id=$IssueId';
-            const values={
-                $name:name,
-                $issueNumber:issueNumber,
-                $publicationDate:publicationDate,
-                $artistId:artistId,
-                $issueId:req.param.issueId
-            };
-            db.run(sql,values,function(err){
-                if(err){
-                    next(err);
-                }else{
-                    db.get(`SELECT * FROM Issue WHERE Issue.Id=${req.param.issueId}`,(err,issue)=>{
-                        res.status(200).json({issue:issue});
-                    });
-                }
-            });                
+issuerouter.put('/:issueId', (req, res, next) => {
+  const name = req.body.issue.name,
+        issueNumber = req.body.issue.issueNumber,
+        publicationDate = req.body.issue.publicationDate,
+        artistId = req.body.issue.artistId;
+  const artistSql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
+  const artistValues = {$artistId: artistId};
+  db.get(artistSql, artistValues, (error, artist) => {
+    if (error) {
+      next(error);
+    } else {
+      if (!name || !issueNumber || !publicationDate || !artist) {
+        return res.sendStatus(400);
+      }
+
+      const sql = 'UPDATE Issue SET name = $name, issue_number = $issueNumber, ' +
+          'publication_date = $publicationDate, artist_id = $artistId ' +
+          'WHERE Issue.id = $issueId';
+      const values = {
+        $name: name,
+        $issueNumber: issueNumber,
+        $publicationDate: publicationDate,
+        $artistId: artistId,
+        $issueId: req.params.issueId
+      };
+
+      db.run(sql, values, function(error) {
+        if (error) {
+          next(error);
+        } else {
+          db.get(`SELECT * FROM Issue WHERE Issue.id = ${req.params.issueId}`,
+            (error, issue) => {
+              res.status(200).json({issue: issue});
+            });
         }
-    });
+      });
+    }
+  });
 });
 
 //delete issue with the specefied issueId
-issuerouter.delete('/:issueId',(req,res,next)=>{
-    const sql='DELETE FROM Issue WHERE Issue.id=$issueId';
-    const values={$issueId:req.param.issueId};
+issuerouter.delete('/:issueId', (req, res, next) => {
+  const sql = 'DELETE FROM Issue WHERE Issue.id = $issueId';
+  const values = {$issueId: req.params.issueId};
 
-    db.run(sql,values,(err)=>{
-        if(err){
-            next(err);
-        }
-        else{
-            res.sendStatus(204);
-        }
-    });
+  db.run(sql, values, (error) => {
+    if (error) {
+      next(error);
+    } else {
+      res.sendStatus(204);
+    }
+  });
 });
 
-module.exports=issuerouter;
+module.exports = issuerouter;
